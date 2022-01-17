4cb37d3976a65448c7cad05f5637aef4e3aa70bd
dudchenko610 <rusland610@.gmail.com>
2022-01-17 18:41:32 +0200

    added provider builder extension for remaining projects

---
+++ ManagedCode.Repository.AzureTable/Extensions/ProviderBuilderExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ManagedCode.Repository.AzureTable.Extensions
+{
+    public static class ProviderBuilderExtension
+    {
+        public static IServiceCollection AddAzureTable(this IServiceCollection serviceCollection, Action<AzureTableRepositoryOptions> action)
+        {
+            var connectionOptions = new AzureTableRepositoryOptions();
+            action.Invoke(connectionOptions);
+
+            serviceCollection.AddSingleton(connectionOptions);
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ ManagedCode.Repository.CosmosDB/Extensions/ProviderBuilderExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ManagedCode.Repository.CosmosDB.Extensions
+{
+    public static class ProviderBuilderExtension
+    {
+        public static IServiceCollection AddCosmoDb(this IServiceCollection serviceCollection, Action<CosmosDbRepositoryOptions> action)
+        {
+            var connectionOptions = new CosmosDbRepositoryOptions();
+            action.Invoke(connectionOptions);
+
+            serviceCollection.AddSingleton(connectionOptions);
+
+            return serviceCollection;
+        }
+    }
+}
