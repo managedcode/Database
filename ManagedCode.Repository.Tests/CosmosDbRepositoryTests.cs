bdd54ebaed245bd7a2ff97f84f51e15a06956e17
ksemenenko <mail@ksemenenko.com>
2022-04-08 19:02:05 +0200

    cosmos2

--- ManagedCode.Repository.Tests/CosmosDbRepositoryTests.cs
+++ ManagedCode.Repository.Tests/CosmosDbRepositoryTests.cs
@@ -10,7 +10,7 @@ namespace ManagedCode.Repository.Tests
     public class CosmosDbRepositoryTests : BaseRepositoryTests<string, TestCosmosDbItem>
     {
         public const string ConnectionString =
-            "AccountEndpoint=https://localhost:3000/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+            "AccountEndpoint=http://localhost:3000/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
         public CosmosDbRepositoryTests() : base(new CosmosDbRepository<TestCosmosDbItem>(
             new CosmosDbRepositoryOptions
