b9a86a379e1f462c68dc90b222137294e802f3a7
ksemenenko <mail@ksemenenko.com>
2022-04-08 10:09:14 +0200

    cosmosdb

--- ManagedCode.Repository.Tests/CosmosDbRepositoryTests.cs
+++ ManagedCode.Repository.Tests/CosmosDbRepositoryTests.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using ManagedCode.Repository.CosmosDB;
@@ -10,7 +10,7 @@ namespace ManagedCode.Repository.Tests
     public class CosmosDbRepositoryTests : BaseRepositoryTests<string, TestCosmosDbItem>
     {
         public const string ConnectionString =
-            "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;";
+            "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
         public CosmosDbRepositoryTests() : base(new CosmosDbRepository<TestCosmosDbItem>(
             new CosmosDbRepositoryOptions
@@ -73,5 +73,5 @@ namespace ManagedCode.Repository.Tests
                 .WithMessage("*Resource Not Found*");
         }
     }
-}*/
+}
 
