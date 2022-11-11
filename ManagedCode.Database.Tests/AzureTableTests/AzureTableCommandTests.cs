21c9500bae822e63ba6a59d27b7519c51932b4f0
tanya_rybina <tryb2516@gmail.com>
2022-11-11 13:41:29 +0200

    overrode tests that behave different for azure tables

--- ManagedCode.Database.Tests/AzureTableTests/AzureTableCommandTests.cs
+++ ManagedCode.Database.Tests/AzureTableTests/AzureTableCommandTests.cs
@@ -1,10 +1,13 @@
 ﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using FluentAssertions;
 using ManagedCode.Database.AzureTable;
 using ManagedCode.Database.Core;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
+using Microsoft.Azure.Cosmos.Table;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ManagedCode.Database.Tests.AzureTableTests
@@ -45,6 +48,40 @@ namespace ManagedCode.Database.Tests.AzureTableTests
         {
             await _azureTableContainer.StartAsync();
             await _database.InitializeAsync();
+        }
+
+        public override async Task UpdateOneItemFromList()
+        {
+            List<TestAzureTableItem> list = new();
+
+            var id = GenerateId();
+            
+            list.Add(CreateNewItem(id));
+            for (var i = 0; i < 9; i++)
+            {
+                list.Add(CreateNewItem());
+            }
+
+            var items = await Collection.InsertAsync(list);
+            list.Clear();
+
+            list.Add(CreateNewItem(id));
+
+            //  ᓚᘏᗢ It throws exception if item with that Id doesn't exists
+            var updatedItems = await Collection.UpdateAsync(list);
+
+            list.Count.Should().Be(1);
+            items.Should().Be(10);
+            updatedItems.Should().Be(1);
+        }
+
+        public override async Task UpdateItem_WhenItem_DoesntExists()
+        {
+            var id = GenerateId();
+
+            var updateItemAction = async () => await Collection.UpdateAsync(CreateNewItem(id));
+
+            await updateItemAction.Should().ThrowExactlyAsync<StorageException>();
         }
     }
 }
