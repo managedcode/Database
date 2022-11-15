46c373ed067d7759f2fc2e75b6ce85e18a2673b6
Oleksii Sokol <sokol@managed-code.com>
2022-11-15 12:20:07 +0200

    Fixed CosmosDBCollectionQueryable

--- ManagedCode.Database.CosmosDB/CosmosDBCollectionQueryable.cs
+++ ManagedCode.Database.CosmosDB/CosmosDBCollectionQueryable.cs
@@ -10,7 +10,7 @@ using ManagedCode.Database.CosmosDB.Extensions;
 
 namespace ManagedCode.Database.CosmosDB;
 
-public class CosmosDBCollectionQueryable<TItem> : OldBaseDBCollectionQueryable<TItem>
+public class CosmosDBCollectionQueryable<TItem> : BaseDBCollectionQueryable<TItem>
     where TItem : CosmosDBItem, IItem<string>, new()
 {
     private readonly IQueryable<TItem> _query;
@@ -20,16 +20,73 @@ public class CosmosDBCollectionQueryable<TItem> : OldBaseDBCollectionQueryable<T
         _query = query;
     }
 
+    private IQueryable<TItem> GetItemsInternal()
+    {
+        var cosmosQuery = _query;
+
+        foreach (var query in Predicates)
+        {
+            switch (query.QueryType)
+            {
+                case QueryType.Where:
+                    cosmosQuery = cosmosQuery.Where(x => query.ExpressionBool.Compile().Invoke(x));
+                    break;
+
+                case QueryType.OrderBy:
+                    // TODO: Maybe need to check is IOrderedQueryable and do throw exception
+
+                    cosmosQuery = cosmosQuery.OrderBy(x => query.ExpressionObject.Compile().Invoke(x));
+                    break;
+
+                case QueryType.OrderByDescending:
+                    // TODO: Maybe need to check is IOrderedQueryable and do throw exception
+
+                    cosmosQuery = cosmosQuery.OrderByDescending(x => query.ExpressionObject.Compile().Invoke(x));
+                    break;
+
+                case QueryType.ThenBy:
+                    if (cosmosQuery is IOrderedQueryable<TItem> orderedItems)
+                    {
+                        cosmosQuery = orderedItems.ThenBy(x => query.ExpressionObject.Compile().Invoke(x));
+                        break;
+                    }
+                    throw new InvalidOperationException("Before ThenBy call first OrderBy.");
+
+                case QueryType.ThenByDescending:
+                    if (cosmosQuery is IOrderedQueryable<TItem> orderedDescendingItems)
+                    {
+                        cosmosQuery = orderedDescendingItems.ThenBy(x => query.ExpressionObject.Compile().Invoke(x));
+                        break;
+                    }
+                    throw new InvalidOperationException("Before ThenBy call first OrderBy.");
+
+                case QueryType.Take:
+                    if (query.Count.HasValue)
+                    {
+                        cosmosQuery = cosmosQuery.Take(query.Count.Value);
+                    }
+                    break;
+
+                case QueryType.Skip:
+                    if (query.Count.HasValue)
+                    {
+                        cosmosQuery = cosmosQuery.Skip(query.Count.Value);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return cosmosQuery;
+    }
+
+
     public override async IAsyncEnumerable<TItem> ToAsyncEnumerable(
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        var feedIterator = _query
-            .Where(WherePredicates)
-            .OrderBy(OrderByPredicates)
-            .OrderByDescending(OrderByDescendingPredicates)
-            .Take(TakeValue)
-            .Skip(SkipValue)
-            .ToFeedIterator();
+        var feedIterator = GetItemsInternal().ToFeedIterator();
 
         using (var iterator = feedIterator)
         {
@@ -49,16 +106,12 @@ public class CosmosDBCollectionQueryable<TItem> : OldBaseDBCollectionQueryable<T
     {
         await Task.Yield();
 
-        return _query
-            .Where(WherePredicates)
-            .FirstOrDefault();
+        return GetItemsInternal().FirstOrDefault();
     }
 
     public override async Task<long> CountAsync(CancellationToken cancellationToken = default)
     {
-        return await _query
-            .Where(WherePredicates)
-            .CountAsync(cancellationToken);
+        return await GetItemsInternal().CountAsync(cancellationToken);
     }
 
     public override Task<int> DeleteAsync(CancellationToken cancellationToken = default)
