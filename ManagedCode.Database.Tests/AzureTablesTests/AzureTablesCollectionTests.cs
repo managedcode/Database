201db68e4f7ff5ca9ea5cb7f16e07e9d391f1ec8
tanya_rybina <tryb2516@gmail.com>
2022-11-21 22:00:21 +0200

    removed deleted tests

--- ManagedCode.Database.Tests/AzureTablesTests/AzureTablesCollectionTests.cs
+++ ManagedCode.Database.Tests/AzureTablesTests/AzureTablesCollectionTests.cs
@@ -1,7 +1,12 @@
-﻿using ManagedCode.Database.AzureTables;
+﻿using FluentAssertions;
+using ManagedCode.Database.AzureTables;
+using ManagedCode.Database.Core.Exceptions;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
 using ManagedCode.Database.Tests.TestContainers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ManagedCode.Database.Tests.AzureTablesTests;
 
@@ -9,31 +14,6 @@ public class AzureTablesCollectionTests : BaseCollectionTests<TableId, TestAzure
 {
     public AzureTablesCollectionTests() : base(new AzureTablesTestContainer())
     {
-    }
-
-    public override async Task UpdateListOfItems_WhenOnlyOneItemUpdated()
-    {
-        List<TestAzureTablesItem> list = new();
-
-        var id = GenerateId();
-
-        list.Add(CreateNewItem(id));
-        for (var i = 0; i < 9; i++)
-        {
-            list.Add(CreateNewItem());
-        }
-
-        var items = await Collection.InsertAsync(list);
-        list.Clear();
-
-        list.Add(CreateNewItem(id));
-
-        //  ᓚᘏᗢ It throws exception if item with that Id doesn't exists
-        var updatedItems = await Collection.UpdateAsync(list);
-
-        list.Count.Should().Be(1);
-        items.Should().Be(10);
-        updatedItems.Should().Be(1);
     }
 
     public override async Task UpdateItem_WhenItemDoesntExists()
@@ -46,12 +26,6 @@ public class AzureTablesCollectionTests : BaseCollectionTests<TableId, TestAzure
     {
         var baseMethod = () => base.DeleteListOfItemsById();
         await baseMethod.Should().ThrowExactlyAsync<NotSupportedException>();
-    }
-
-    public override async Task InsertItems_WhenOneItemAlreadyExists()
-    {
-        var baseMethod = () => base.InsertItems_WhenOneItemAlreadyExists();
-        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
     }
 
     public override async Task DeleteListOfItems_WhenItemsDontExist()
@@ -80,11 +54,5 @@ public class AzureTablesCollectionTests : BaseCollectionTests<TableId, TestAzure
         // Assert
         deletedItems.Should().BeTrue();
         await countAction.Should().ThrowExactlyAsync<DatabaseException>();
-    }
-
-    public override async Task DeleteAll_WhenNoItems()
-    {
-        var baseMethod = () => base.DeleteAll_WhenNoItems();
-        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
     }
 }
