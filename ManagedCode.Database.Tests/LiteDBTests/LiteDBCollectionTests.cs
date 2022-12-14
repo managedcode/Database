2862136b462f8b15e042ab30ca27333c464db04e
tanya_rybina <tryb2516@gmail.com>
2022-12-14 20:16:35 +0200

    updated packages

--- ManagedCode.Database.Tests/LiteDBTests/LiteDBCollectionTests.cs
+++ ManagedCode.Database.Tests/LiteDBTests/LiteDBCollectionTests.cs
@@ -12,39 +12,6 @@ public class LiteDBCollectionTests : BaseCollectionTests<string, TestLiteDBItem>
 {
     public LiteDBCollectionTests() : base(new LiteDBTestContainer())
     {
-    }
 
-    public override async Task InsertOrUpdateListOfItems()
-    {
-        int itemsCount = 5;
-        List<TestLiteDBItem> items = new List<TestLiteDBItem>();
-
-        for (int i = 0; i < itemsCount; i++)
-        {
-            items.Add(CreateNewItem());
-        }
-        
-        var itemsInsert = await Collection.InsertOrUpdateAsync(items);
-
-        for (int i = 0; i < itemsCount; i++)
-        {
-            items[i] = CreateNewItem(items[i].Id);
-        }
-        
-        // Act
-        var itemsUpdate = 0;
-
-        foreach (var item in items)
-        {
-            var upsertResult = await Collection.InsertOrUpdateAsync(item);
-            if (upsertResult != null)
-                itemsUpdate++;
-        }
-        
-        
-        // Assert
-        itemsUpdate.Should().Be(itemsCount);
-        itemsInsert.Should().Be(itemsCount);
-        items.Count.Should().Be(itemsCount);
     }
 }
