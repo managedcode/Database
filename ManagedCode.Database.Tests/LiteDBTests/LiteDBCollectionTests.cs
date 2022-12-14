5ab09a350bc77cc82f1a8d6107787b57d183ebf2
tanya_rybina <tryb2516@gmail.com>
2022-12-14 20:50:03 +0200

    fix update or insert LiteDb test

--- ManagedCode.Database.Tests/LiteDBTests/LiteDBCollectionTests.cs
+++ ManagedCode.Database.Tests/LiteDBTests/LiteDBCollectionTests.cs
@@ -14,4 +14,34 @@ public class LiteDBCollectionTests : BaseCollectionTests<string, TestLiteDBItem>
     {
 
     }
+
+    public override async Task InsertOrUpdateListOfItems()
+    {
+
+        // Arrange
+        int itemsCount = 5;
+        int updatedItemsCount = 0;
+        List<TestLiteDBItem> list = new();
+
+        for (var i = 0; i < itemsCount; i++)
+        {
+            list.Add(CreateNewItem());
+        }
+
+        var itemsInsert = await Collection.InsertOrUpdateAsync(list);
+
+        foreach (var item in list)
+        {
+            item.DateTimeData = DateTime.Now.AddDays(-1);
+        }
+
+        // Act
+        var itemsUpdate = await Collection.InsertOrUpdateAsync(list);
+        //TODO: LiteDB must be 100, but result 0
+
+        // Assert
+        itemsUpdate.Should().Be(updatedItemsCount);
+        itemsInsert.Should().Be(itemsCount);
+        list.Count.Should().Be(itemsCount);
+    }
 }
