7f6fd4888b579f84a84a8539f6c90b5a6c8fbfbb
TRybina132 <tryb2516@gmail.com>
2022-12-15 11:50:08 +0200

    change OrderBy_AfterIOrdered_ReturnException() lite db

--- ManagedCode.Database.Tests/LiteDBTests/LiteDBQueryableTests.cs
+++ ManagedCode.Database.Tests/LiteDBTests/LiteDBQueryableTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
 using ManagedCode.Database.Tests.TestContainers;
@@ -8,5 +11,24 @@ public class LiteDBQueryableTests : BaseQueryableTests<string, TestLiteDBItem>
 {
     public LiteDBQueryableTests() : base(new LiteDBTestContainer())
     {
+    }
+
+    public override async Task OrderBy_AfterIOrdered_ReturnException()
+    {
+        // Arrange
+        int itemsCountToInsert = 1;
+
+        await CreateAndInsertItemsAsync(itemsCountToInsert);
+
+        // Act
+        var itemsResult =
+           await Collection.Query
+            .OrderBy(o => o.IntData)
+            .OrderBy(o => o.StringData)
+            .ToListAsync();
+
+        // Assert
+        itemsResult.Should().NotBeNull();
+        itemsResult.Count.Should().Be(itemsCountToInsert);
     }
 }
