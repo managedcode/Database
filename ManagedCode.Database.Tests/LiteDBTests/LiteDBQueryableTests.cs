addbb9fec3b2da151124d605daebe5b4be6e1b81
TRybina132 <tryb2516@gmail.com>
2022-12-15 14:45:23 +0200

    throw exception if orderby already on query

--- ManagedCode.Database.Tests/LiteDBTests/LiteDBQueryableTests.cs
+++ ManagedCode.Database.Tests/LiteDBTests/LiteDBQueryableTests.cs
@@ -12,23 +12,4 @@ public class LiteDBQueryableTests : BaseQueryableTests<string, TestLiteDBItem>
     public LiteDBQueryableTests() : base(new LiteDBTestContainer())
     {
     }
-
-    public override async Task OrderBy_AfterIOrdered_ReturnException()
-    {
-        // Arrange
-        int itemsCountToInsert = 1;
-
-        await CreateAndInsertItemsAsync(itemsCountToInsert);
-
-        // Act
-        var itemsResult =
-           await Collection.Query
-            .OrderBy(o => o.IntData)
-            .OrderBy(o => o.StringData)
-            .ToListAsync();
-
-        // Assert
-        itemsResult.Should().NotBeNull();
-        itemsResult.Count.Should().Be(itemsCountToInsert);
-    }
 }
