a0c86eb2a9c172cf88678b98f40f817c762ca6d6
TRybina132 <tryb2516@gmail.com>
2022-12-20 16:00:22 +0200

    added multithreading base test

--- ManagedCode.Database.Tests/BaseTests/BaseMultiThreadingTests.cs
+++ ManagedCode.Database.Tests/BaseTests/BaseMultiThreadingTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using ManagedCode.Database.Tests.TestContainers;
 using Xunit;
 using System.Collections.Generic;
+using System.Threading;
 using FluentAssertions;
 
 namespace ManagedCode.Database.Tests.BaseTests;
@@ -37,26 +38,85 @@ public abstract class BaseMultiThreadingTests<TId, TItem> : BaseTests<TId, TItem
         return item;
     }
 
-    #region Insert
-
-    [Fact]
-    public virtual async Task InsertFromMultiplieThreads_ReturnsOk()
+    protected IEnumerable<TItem> CreateListOfItems(int itemsCount)
     {
-        // Arrange
-        int taskCount = 100;
-        List<Task> tasks = new List<Task>();
-
-        for(int i = 0; i < taskCount; i++)
-            tasks.Add(Task.Run(async () => await Collection.InsertAsync(CreateNewItem())));
-
-        // Act
-        var act = async () => await Task.WhenAll(tasks);
-
-        //Assert
-        await act.Should().NotThrowAsync();
-        long count = await Collection.CountAsync();
-        count.Should().Be(taskCount);
+        for (int i = 0; i < itemsCount; i++)
+            yield return CreateNewItem();
     }
 
+    #region Insert
+
+    // [Fact]
+    // public virtual async Task InsertFromMultiplyThreads_ReturnsOk()
+    // {
+    //     // Arrange
+    //     int taskCount = 100;
+    //     List<Task> tasks = new List<Task>();
+    //
+    //     for(int i = 0; i < taskCount; i++)
+    //         tasks.Add(Task.Run(async () => await Collection.InsertAsync(CreateNewItem())));
+    //
+    //     // Act
+    //     var act = async () => await Task.WhenAll(tasks);
+    //
+    //     //Assert
+    //     await act.Should().NotThrowAsync();
+    //     long count = await Collection.CountAsync();
+    //     count.Should().Be(taskCount);
+    // }
+
     #endregion
+
+    #region Concurency
+
+    [Fact]
+    public virtual async Task MutateCollectionByManyThreads_ThrowNoException()
+    {
+        // Arrange
+        int threadsCount = 100;
+        int iterations = 300;
+        int itemsToInsertCount = 100;
+        TItem itemToDelete = CreateNewItem();
+        TItem itemToFound = CreateNewItem();
+        
+        List<Thread> threads = new List<Thread>();
+
+        for (int i = 0; i < threadsCount; i++)
+        {
+            Thread thread = new Thread( async () =>
+            {
+                for (int j = 0; j < iterations; j++)
+                {
+                    await Collection.InsertAsync(itemToFound);
+                    await Collection.InsertAsync(itemToDelete);
+                    await Collection.CountAsync();
+                    await Collection.InsertAsync(CreateListOfItems(itemsToInsertCount));
+                    for (int k = 0; k < itemsToInsertCount; k++)
+                    {
+                        await Collection.InsertAsync(CreateNewItem());
+                    }
+                    await Collection.DeleteAsync(itemToDelete.Id);
+                    await Collection.Query.Where(item => item.StringData == itemToFound.StringData).ToListAsync();
+                    await Collection.Query.OrderBy(item => item.IntData).ToListAsync();
+                }
+            });
+            thread.Start();
+            threads.Add(thread);
+        }
+
+        // Act
+        var act = () =>
+        {
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        };
+
+        // Assert
+        act.Should().NotThrow();
+    }
+
+
+    #endregion Concurency
 }
