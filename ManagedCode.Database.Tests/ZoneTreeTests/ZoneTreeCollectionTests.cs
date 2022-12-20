fe045a2da9306e55204cbb5e8cbcc05d4db8706a
TRybina132 <tryb2516@gmail.com>
2022-12-20 12:55:55 +0200

    override upsert zonetree test

--- ManagedCode.Database.Tests/ZoneTreeTests/ZoneTreeCollectionTests.cs
+++ ManagedCode.Database.Tests/ZoneTreeTests/ZoneTreeCollectionTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
 using ManagedCode.Database.Tests.TestContainers;
@@ -8,5 +12,35 @@ public class ZoneTreeCollectionTests : BaseCollectionTests<string, TestZoneTreeI
 {
     public ZoneTreeCollectionTests() : base(new ZoneTreeTestContainer())
     {
+    }
+    
+    public override async Task InsertOrUpdateListOfItems()
+    {
+        // Arrange
+        int itemsCount = 5;
+        int updatedItemsCount = 0;
+        List<TestZoneTreeItem> list = new();
+
+        for (var i = 0; i < itemsCount; i++)
+        {
+            list.Add(CreateNewItem());
+        }
+
+        var itemsInsert = await Collection.InsertOrUpdateAsync(list);
+
+        foreach (var item in list)
+        {
+            item.DateTimeData = DateTime.Now.AddDays(-1);
+            item.StringData = "Updated";
+        }
+
+        // Act
+        var itemsUpdate = await Collection.InsertOrUpdateAsync(list);
+        //TODO: LiteDB must be 100, but result 0
+
+        // Assert
+        itemsUpdate.Should().Be(updatedItemsCount);
+        itemsInsert.Should().Be(itemsCount);
+        list.Count.Should().Be(itemsCount);
     }
 }
