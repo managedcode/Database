450f1029e4571ca5b600f898f25fba71e42fbd89
tanya_rybina <tryb2516@gmail.com>
2022-12-22 12:09:37 +0200

    override test

--- ManagedCode.Database.Tests/CosmosTests/CosmosCollectionTests.cs
+++ ManagedCode.Database.Tests/CosmosTests/CosmosCollectionTests.cs
@@ -1,6 +1,9 @@
+using FluentAssertions;
+using ManagedCode.Database.Core.Exceptions;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
 using ManagedCode.Database.Tests.TestContainers;
+using System.Threading.Tasks;
 
 namespace ManagedCode.Database.Tests.CosmosTests;
 
@@ -8,5 +11,12 @@ public class CosmosCollectionTests : BaseCollectionTests<string, TestCosmosItem>
 {
     public CosmosCollectionTests() : base(new CosmosTestContainer())
     {
+    }
+
+    public override async Task DeleteItemById_WhenItemDoesntExists()
+    {
+        var baseMethod = async () => await base.DeleteItemById_WhenItemDoesntExists();
+
+        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
     }
 }
