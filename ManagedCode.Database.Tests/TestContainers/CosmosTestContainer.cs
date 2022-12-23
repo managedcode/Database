84592dd42956963862e0e1145613bcf365c8e072
ksemenenko <mail@ksemenenko.com>
2022-12-23 23:38:39 +0100

    cosmos

--- ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
+++ ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
@@ -12,23 +12,30 @@ namespace ManagedCode.Database.Tests.TestContainers;
 
 public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>
 {
-    private static int _port = 20000;
+
 
     private readonly CosmosDatabase _database;
     private readonly TestcontainersContainer _cosmosContainer;
 
     public CosmosTestContainer()
     {
-        var port = ++_port;
 
         // Docker container for cosmos db is not working at all, to test database use local windows emulator
-        //_cosmosContainer = new TestcontainersBuilder<TestcontainersContainer>()
-        //    .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest")
-        //    .WithPortBinding(port, 8081)
-        //    .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "30")
-        //    .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
-        //    .WithWaitStrategy(Wait.ForUnixContainer())
-        //    .Build();
+        _cosmosContainer = new TestcontainersBuilder<TestcontainersContainer>()
+            .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator")
+            .WithName("azure-cosmos-emulator")
+            .WithExposedPort(8081)
+            .WithPortBinding(8081, true)
+            .WithPortBinding(10251, true)
+            .WithPortBinding(10252, true)
+            .WithPortBinding(10253, true)
+            .WithPortBinding(10254, true)
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "1")
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE", "127.0.0.1")
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
+            .WithWaitStrategy(Wait.ForUnixContainer()
+                .UntilPortIsAvailable(8081))
+            .Build();
 
         _database = new CosmosDatabase(new CosmosOptions
         {
@@ -43,16 +50,42 @@ public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>
                 {
                     HttpMessageHandler httpMessageHandler = new HttpClientHandler()
                     {
-                        ServerCertificateCustomValidationCallback =
-                            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                        ServerCertificateCustomValidationCallback =  (_, _, _, _) => true
                     };
-
+                    
                     return new HttpClient(httpMessageHandler);
                 },
                 ConnectionMode = ConnectionMode.Gateway
             },
         });
     }
+    
+    /*
+    await Task.Delay(TimeSpan.FromSeconds(30))
+    .ConfigureAwait(false);
+
+        using (var handler = new HttpClientHandler())
+    {
+        handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+
+        using (var client = new HttpClient(handler))
+        {
+            var mappedPort = this.container.GetMappedPublicPort(8081);
+
+            var response = await client.GetAsync($"https://localhost:{mappedPort}/_explorer/emulator.pem")
+                .ConfigureAwait(false);
+
+            var pem = await response.Content.ReadAsStringAsync()
+                .ConfigureAwait(false);
+
+            Debug.WriteLine(pem);
+        }
+    }
+    
+    
+    
+    */
+    
 
     public IDatabaseCollection<string, TestCosmosItem> Collection =>
         _database.GetCollection<TestCosmosItem>();
