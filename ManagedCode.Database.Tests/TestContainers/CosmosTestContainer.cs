4405e6c8629a5f8634aacb5b3fdaf0177e32c2ba
ksemenenko <mail@ksemenenko.com>
2022-12-24 10:00:00 +0100

    cleanup

--- ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
+++ ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
@@ -22,7 +22,6 @@ public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>
 
         // Docker container for cosmos db is not working at all, to test database use local windows emulator
         _cosmosContainer = new TestcontainersBuilder<TestcontainersContainer>()
-            .WithCleanUp(true)
             .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator")
             .WithName("azure-cosmos-emulator")
             .WithExposedPort(8081)
@@ -36,6 +35,7 @@ public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>
             .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
             .WithWaitStrategy(Wait.ForUnixContainer()
             .UntilPortIsAvailable(8081))
+            .WithCleanUp(true)
             .Build();
         
         _database = new CosmosDatabase(new CosmosOptions
