7fe23e744b96693d4dffd8decb05fa5529e1bfd8
Oleksii Sokol <sokol@managed-code.com>
2022-12-28 16:11:46 +0200

    Fix delete for DynamoDb

--- ManagedCode.Database.DynamoDB/DynamoDBCollection.cs
+++ ManagedCode.Database.DynamoDB/DynamoDBCollection.cs
@@ -3,6 +3,8 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.Model.Internal.MarshallTransformations;
+using Amazon.Runtime.Internal;
 using ManagedCode.Database.Core;
 using ManagedCode.Database.Core.Exceptions;
 using System;
@@ -14,6 +16,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Transactions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace ManagedCode.Database.DynamoDB;
 
@@ -49,6 +52,19 @@ public class DynamoDBCollection<TItem> : BaseDatabaseCollection<string, TItem>
         return new List<ScanCondition>()
         {
             new ScanCondition("Id", ScanOperator.Equal, id)
+        };
+    }
+
+    private DeleteItemRequest DeleteItemRequestById(string id)
+    {
+        return new DeleteItemRequest
+        {
+            TableName = _tableName,
+            Key = new Dictionary<string, AttributeValue>()
+                    {
+                        {"Id", new AttributeValue() {S = id}}
+                    },
+            ReturnValues = "ALL_OLD",
         };
     }
 
@@ -101,7 +117,7 @@ public class DynamoDBCollection<TItem> : BaseDatabaseCollection<string, TItem>
     {
         var batchWriter = _dynamoDBContext.CreateBatchWrite<TItem>(_config);
 
-         batchWriter.AddPutItems(items);
+        batchWriter.AddPutItems(items);
 
         await batchWriter.ExecuteAsync();
 
@@ -180,76 +196,98 @@ public class DynamoDBCollection<TItem> : BaseDatabaseCollection<string, TItem>
 
     protected override async Task<bool> DeleteInternalAsync(TItem item, CancellationToken cancellationToken = default)
     {
-        await _dynamoDBContext.DeleteAsync(item, _config, cancellationToken);
+        var response = await _dynamoDBClient.DeleteItemAsync(DeleteItemRequestById(item.Id), cancellationToken);
 
-        var data = await _dynamoDBContext.ScanAsync<TItem>(GetScanConditions(item), _config).GetRemainingAsync(cancellationToken);
+        if (response.Attributes.Count != 0 && response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return true;
+        }
 
-        return data.Count == 0 ? true : false;
+        return false;
     }
 
     protected override async Task<int> DeleteInternalAsync(IEnumerable<TItem> items,
         CancellationToken cancellationToken = default)
     {
-        var batchWriter = _dynamoDBContext.CreateBatchWrite<TItem>(_config);
+        var count = 0;
 
-        batchWriter.AddDeleteItems(items);
+        IEnumerable<TItem[]> itemsChunk = items.Chunk(100);
 
-        await batchWriter.ExecuteAsync();
+        foreach (var itemsList in itemsChunk)
+        {
+            var tasks = new List<Task>();
 
-        return items.Count(); //TODO check count of added items
+            foreach (var item in itemsList)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    var response = await _dynamoDBClient.DeleteItemAsync(DeleteItemRequestById(item.Id), cancellationToken);
+
+                    if (response.Attributes.Count != 0 && response.HttpStatusCode == System.Net.HttpStatusCode.OK) 
+                        Interlocked.Increment(ref count);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        return count;
     }
 
-    protected override async Task<bool> DeleteCollectionInternalAsync(CancellationToken cancellationToken = default)
+protected override async Task<bool> DeleteCollectionInternalAsync(CancellationToken cancellationToken = default)
     {
         DeleteTableRequest deleteTableRequest = new DeleteTableRequest
         {
             TableName = _tableName,
         };
-        
-        await _dynamoDBClient.DeleteTableAsync(deleteTableRequest);
 
-        bool result = false;
+        var response = await _dynamoDBClient.DeleteTableAsync(deleteTableRequest);
 
-        try
+        if(response.HttpStatusCode == System.Net.HttpStatusCode.OK)
         {
-            Table.LoadTable(_dynamoDBClient, _tableName); //TODO Edit check table
-        }
-        catch
-        {
-            result = true;
+            return true;
         }
 
-        return result;
+        return false;
     }
 
     protected override async Task<bool> DeleteInternalAsync(string id, CancellationToken cancellationToken = default)
     {
-        var data = await _dynamoDBContext.ScanAsync<TItem>(GetScanConditions(id), _config).GetRemainingAsync(cancellationToken);
+        var response = await _dynamoDBClient.DeleteItemAsync(DeleteItemRequestById(id), cancellationToken);
 
-        await _dynamoDBContext.DeleteAsync(data.FirstOrDefault(), _config, cancellationToken);
+        if (response.Attributes.Count != 0 && response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return true;
+        }
 
-        data = await _dynamoDBContext.ScanAsync<TItem>(GetScanConditions(id), _config).GetRemainingAsync(cancellationToken);
-
-        return data.Count == 0 ? true : false;
+        return false;
     }
 
     protected override async Task<int> DeleteInternalAsync(IEnumerable<string> ids, CancellationToken cancellationToken = default)
     {
-        var data = new List<TItem>();
+        var count = 0;
 
-        foreach(var id in ids)
+        IEnumerable<string[]> idsChunk = ids.Chunk(100);
+
+        foreach (var idsList in idsChunk)
         {
-            var item = await _dynamoDBContext.ScanAsync<TItem>(GetScanConditions(id), _config).GetRemainingAsync(cancellationToken);
-            data.Add(item.First());
+            var tasks = new List<Task>();
+
+            foreach (var id in idsList)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    var response = await _dynamoDBClient.DeleteItemAsync(DeleteItemRequestById(id), cancellationToken);
+
+                    if (response.Attributes.Count != 0 && response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                        Interlocked.Increment(ref count);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
         }
 
-        var batchWriter = _dynamoDBContext.CreateBatchWrite<TItem>(_config);
-
-        batchWriter.AddDeleteItems(data);
-
-        await batchWriter.ExecuteAsync();
-
-        return ids.Count(); //TODO check count of added items
+        return count;
     }
 
     #endregion
--- ManagedCode.Database.Tests/DynamoDbTests/DynamoDbCollectionTests.cs
+++ ManagedCode.Database.Tests/DynamoDbTests/DynamoDbCollectionTests.cs
@@ -1,7 +1,14 @@
 using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using FluentAssertions;
+using ManagedCode.Database.Core.Exceptions;
 using ManagedCode.Database.Tests.BaseTests;
 using ManagedCode.Database.Tests.Common;
 using ManagedCode.Database.Tests.TestContainers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace ManagedCode.Database.Tests.DynamoDbTests;
 
@@ -9,5 +16,39 @@ public class DynamoDbCollectionTests : BaseCollectionTests<string, TestDynamoDbI
 {
     public DynamoDbCollectionTests() : base(new DynamoDBTestContainer())
     {
+    }
+
+    [Fact]
+    public override async Task DeleteAll()
+    {
+        // Arrange
+        int itemsCount = 5;
+        List<TestDynamoDbItem> list = new();
+
+        for (var i = 0; i < itemsCount; i++)
+        {
+            list.Add(CreateNewItem());
+        }
+
+        await Collection.InsertAsync(list);
+
+        // Act
+        var deletedItems = await Collection.DeleteCollectionAsync();
+
+        // Assert
+        deletedItems.Should().BeTrue();
+    }
+
+    [Fact]
+    public override async Task DeleteItemById_WhenItemDoesntExists()
+    {
+        // Arrange
+        var item = CreateNewItem();
+
+        // Act
+        var deleted = () => Collection.DeleteAsync(item.Id);
+
+        // Assert
+        deleted.Should().ThrowExactlyAsync<DatabaseException>();
     }
 }
