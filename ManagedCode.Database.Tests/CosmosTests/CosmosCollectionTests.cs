ea5cf41787fa21bb04a4c7c68d2e19a6be977040
ksemenenko <mail@ksemenenko.com>
2022-12-28 23:28:05 +0100

    onlu mongo

--- ManagedCode.Database.Tests/CosmosTests/CosmosCollectionTests.cs
+++ ManagedCode.Database.Tests/CosmosTests/CosmosCollectionTests.cs
@@ -1,37 +1,37 @@
-using FluentAssertions;
-using ManagedCode.Database.Core.Exceptions;
-using ManagedCode.Database.Tests.BaseTests;
-using ManagedCode.Database.Tests.Common;
-using ManagedCode.Database.Tests.TestContainers;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace ManagedCode.Database.Tests.CosmosTests;
-
-public class CosmosCollectionTests : BaseCollectionTests<string, TestCosmosItem>,  IClassFixture<CosmosTestContainer>
-{
-    public CosmosCollectionTests(CosmosTestContainer container) : base(container)
-    {
-    }
-
-    public override async Task DeleteItemById_WhenItemDoesntExists()
-    {
-        var baseMethod = async () => await base.DeleteItemById_WhenItemDoesntExists();
-
-        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
-    }
-
-    public override async Task DeleteListOfItemsById_WhenItemsDontExist()
-    {
-        var baseMethod = async () => await base.DeleteListOfItemsById_WhenItemsDontExist();
-
-        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
-    }
-
-    public override async Task DeleteListOfItems_WhenItemsDontExist()
-    {
-        var baseMethod = async () => await base.DeleteListOfItems_WhenItemsDontExist();
-
-        await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
-    }
-}
+// using FluentAssertions;
+// using ManagedCode.Database.Core.Exceptions;
+// using ManagedCode.Database.Tests.BaseTests;
+// using ManagedCode.Database.Tests.Common;
+// using ManagedCode.Database.Tests.TestContainers;
+// using System.Threading.Tasks;
+// using Xunit;
+//
+// namespace ManagedCode.Database.Tests.CosmosTests;
+//
+// public class CosmosCollectionTests : BaseCollectionTests<string, TestCosmosItem>,  IClassFixture<CosmosTestContainer>
+// {
+//     public CosmosCollectionTests(CosmosTestContainer container) : base(container)
+//     {
+//     }
+//
+//     public override async Task DeleteItemById_WhenItemDoesntExists()
+//     {
+//         var baseMethod = async () => await base.DeleteItemById_WhenItemDoesntExists();
+//
+//         await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
+//     }
+//
+//     public override async Task DeleteListOfItemsById_WhenItemsDontExist()
+//     {
+//         var baseMethod = async () => await base.DeleteListOfItemsById_WhenItemsDontExist();
+//
+//         await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
+//     }
+//
+//     public override async Task DeleteListOfItems_WhenItemsDontExist()
+//     {
+//         var baseMethod = async () => await base.DeleteListOfItems_WhenItemsDontExist();
+//
+//         await baseMethod.Should().ThrowExactlyAsync<DatabaseException>();
+//     }
+// }
--- ManagedCode.Database.Tests/CosmosTests/CosmosQueryableTests.cs
+++ ManagedCode.Database.Tests/CosmosTests/CosmosQueryableTests.cs
@@ -1,109 +1,109 @@
-using FluentAssertions;
-using ManagedCode.Database.Tests.BaseTests;
-using ManagedCode.Database.Tests.Common;
-using ManagedCode.Database.Tests.TestContainers;
-using System.Threading.Tasks;
-using System;
-using Xunit;
-
-namespace ManagedCode.Database.Tests.CosmosTests;
-
-public class CosmosQueryableTests : BaseQueryableTests<string, TestCosmosItem>, IClassFixture<CosmosTestContainer>
-{
-    public CosmosQueryableTests(CosmosTestContainer container) : base(container)
-    {
-    }
-
-    public override async Task OrderBy_TakeNull_ReturnException()
-    {
-        // Arrange
-        int itemsCountToInsert = 1;
-
-        var guid = Guid.NewGuid().ToString();
-
-        await CreateAndInsertItemsAsync(itemsCountToInsert);
-
-        // Act
-        var itemsResult = () => Collection.Query
-            .OrderBy(null)
-            .ToListAsync();
-
-        // Assert
-        await itemsResult
-            .Should()
-            .ThrowAsync<ArgumentNullException>();
-    }
-
-    public override async Task OrderByDescending_TakeNull_ReturnException()
-    {
-        // Arrange
-        int itemsCountToInsert = 1;
-
-        var guid = Guid.NewGuid().ToString();
-        var unfaithfulGuid = Guid.NewGuid().ToString();
-
-        await CreateAndInsertItemsAsync(itemsCountToInsert, guid);
-
-        // Act
-        var itemsResult = () => Collection.Query
-            .OrderByDescending(null)
-            .ToListAsync();
-
-        // Assert
-        await itemsResult
-            .Should()
-            .ThrowAsync<ArgumentNullException>();
-    }
-
-    public override async Task Where_TakeNull_ReturnException()
-    {
-        // Arrange
-        int itemsCountToInsert = 1;
-
-
-        await CreateAndInsertItemsAsync(itemsCountToInsert);
-
-        // Act
-        var itemsResult = () => Collection.Query
-            .Where(null)
-            .ToListAsync();
-
-        // Assert
-        await itemsResult
-            .Should()
-            .ThrowAsync<ArgumentNullException>();
-    }
-
-    public override async Task ThenByDescending_TakeNull_ReturnException()
-    {
-        // Arrange
-        int itemsCountToInsert = 1;
-
-        await CreateAndInsertItemsAsync(itemsCountToInsert);
-
-        // Act
-        var itemsResult = () => Collection.Query
-            .OrderBy(o => o.StringData)
-            .ThenByDescending(null)
-            .ToListAsync();
-
-        // Assert
-        await itemsResult
-            .Should()
-            .ThrowAsync<ArgumentNullException>();
-    }
-
-    public override async Task Skip_NegativeNumber_ReturnZero()
-    {
-        var baseMethod = async () => await base.Skip_NegativeNumber_ReturnZero();
-
-        await baseMethod.Should().ThrowExactlyAsync<ArgumentException>();
-    }
-
-    public override async Task Take_NegativeNumber_ReturnZero()
-    {
-        var baseMethod = async () => await base.Take_NegativeNumber_ReturnZero();
-
-        await baseMethod.Should().ThrowExactlyAsync<ArgumentException>();
-    }
-}
+// using FluentAssertions;
+// using ManagedCode.Database.Tests.BaseTests;
+// using ManagedCode.Database.Tests.Common;
+// using ManagedCode.Database.Tests.TestContainers;
+// using System.Threading.Tasks;
+// using System;
+// using Xunit;
+//
+// namespace ManagedCode.Database.Tests.CosmosTests;
+//
+// public class CosmosQueryableTests : BaseQueryableTests<string, TestCosmosItem>, IClassFixture<CosmosTestContainer>
+// {
+//     public CosmosQueryableTests(CosmosTestContainer container) : base(container)
+//     {
+//     }
+//
+//     public override async Task OrderBy_TakeNull_ReturnException()
+//     {
+//         // Arrange
+//         int itemsCountToInsert = 1;
+//
+//         var guid = Guid.NewGuid().ToString();
+//
+//         await CreateAndInsertItemsAsync(itemsCountToInsert);
+//
+//         // Act
+//         var itemsResult = () => Collection.Query
+//             .OrderBy(null)
+//             .ToListAsync();
+//
+//         // Assert
+//         await itemsResult
+//             .Should()
+//             .ThrowAsync<ArgumentNullException>();
+//     }
+//
+//     public override async Task OrderByDescending_TakeNull_ReturnException()
+//     {
+//         // Arrange
+//         int itemsCountToInsert = 1;
+//
+//         var guid = Guid.NewGuid().ToString();
+//         var unfaithfulGuid = Guid.NewGuid().ToString();
+//
+//         await CreateAndInsertItemsAsync(itemsCountToInsert, guid);
+//
+//         // Act
+//         var itemsResult = () => Collection.Query
+//             .OrderByDescending(null)
+//             .ToListAsync();
+//
+//         // Assert
+//         await itemsResult
+//             .Should()
+//             .ThrowAsync<ArgumentNullException>();
+//     }
+//
+//     public override async Task Where_TakeNull_ReturnException()
+//     {
+//         // Arrange
+//         int itemsCountToInsert = 1;
+//
+//
+//         await CreateAndInsertItemsAsync(itemsCountToInsert);
+//
+//         // Act
+//         var itemsResult = () => Collection.Query
+//             .Where(null)
+//             .ToListAsync();
+//
+//         // Assert
+//         await itemsResult
+//             .Should()
+//             .ThrowAsync<ArgumentNullException>();
+//     }
+//
+//     public override async Task ThenByDescending_TakeNull_ReturnException()
+//     {
+//         // Arrange
+//         int itemsCountToInsert = 1;
+//
+//         await CreateAndInsertItemsAsync(itemsCountToInsert);
+//
+//         // Act
+//         var itemsResult = () => Collection.Query
+//             .OrderBy(o => o.StringData)
+//             .ThenByDescending(null)
+//             .ToListAsync();
+//
+//         // Assert
+//         await itemsResult
+//             .Should()
+//             .ThrowAsync<ArgumentNullException>();
+//     }
+//
+//     public override async Task Skip_NegativeNumber_ReturnZero()
+//     {
+//         var baseMethod = async () => await base.Skip_NegativeNumber_ReturnZero();
+//
+//         await baseMethod.Should().ThrowExactlyAsync<ArgumentException>();
+//     }
+//
+//     public override async Task Take_NegativeNumber_ReturnZero()
+//     {
+//         var baseMethod = async () => await base.Take_NegativeNumber_ReturnZero();
+//
+//         await baseMethod.Should().ThrowExactlyAsync<ArgumentException>();
+//     }
+// }
