9a4457177aca4b36aa327e842a434101ab73e6dc
Oleksii Sokol <sokol@managed-code.com>
2023-01-12 20:26:32 +0200

    cosmos tests

--- ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
+++ ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
@@ -99,7 +99,7 @@ public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>,
             ConnectionString =
                 $"AccountEndpoint=https://localhost:{publicPort}/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
             DatabaseName = "database",
-            CollectionName = db,
+            CollectionName = "testContainer",
             AllowTableCreation = true,
             CosmosClientOptions = new CosmosClientOptions()
             {
