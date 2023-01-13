b8a7af9b969a67458d23ee25675b1372f8b0295a
Oleksii Sokol <sokol@managed-code.com>
2023-01-13 13:14:26 +0200

    fix

--- ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
+++ ManagedCode.Database.Tests/TestContainers/CosmosTestContainer.cs
@@ -98,7 +98,7 @@ public class CosmosTestContainer : ITestContainer<string, TestCosmosItem>,
         _database = new CosmosDatabase(new CosmosOptions
         {
             ConnectionString =
-                $"AccountEndpoint=https://localhost:{publicPort}/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                $"AccountEndpoint=https://localhost:8081;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
             DatabaseName = "database",
             CollectionName = $"testContainer",
             AllowTableCreation = true,
